5df18403a848cb90b6e3c9148292b76479493596
advokat11 <adwaitaman3@gmail.com>
2023-10-04 13:19:36 +0300

    Removed multithreading
    Added Russian language

--- main.go
+++ main.go
@@ -2,21 +2,53 @@ package main
 
 import (
 	"fmt"
-	"github.com/schollz/progressbar/v3"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"sync"
-	"sync/atomic"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
-	args := os.Args[1:]
-	loggingEnabled := len(args) > 0 && args[0] == "log"
+
+	driverFiles, err := findDriverFiles()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bar := progressbar.Default(int64(len(driverFiles)))
+
+	var successfulInstalls uint64
+	var failedInstalls uint64
+
+	for _, path := range driverFiles {
+
+		driverName := filepath.Base(path)
+		msg := fmt.Sprintf("Устанавливаемый драйвер: %s", driverName)
+
+		bar.Describe(msg)
+
+		if installDriver(path) != nil {
+			failedInstalls++
+		} else {
+			successfulInstalls++
+		}
+
+		bar.Add(1)
+	}
+
+	bar.Finish()
+
+	printStats(successfulInstalls, failedInstalls)
+
+}
+
+func findDriverFiles() ([]string, error) {
 
 	var driverFiles []string
+
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -26,52 +58,25 @@ func main() {
 		}
 		return nil
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	numDrivers := int64(len(driverFiles))
-	bar := progressbar.Default(numDrivers)
+	return driverFiles, err
 
-	var successfulInstalls uint64
-	var failedInstalls uint64
-	var wg sync.WaitGroup
+}
 
-	var logFile *os.File
-	if loggingEnabled {
-		logFile, _ = os.Create("log.txt")
-		defer logFile.Close()
-	}
+func installDriver(path string) error {
 
-	for _, path := range driverFiles {
-		driverName := filepath.Base(path)
-		msg := fmt.Sprintf("Installing driver: %s", driverName)
-		bar.Describe(msg)
+	cmd := exec.Command("pnputil", "/add-driver", path, "/install")
+	err := cmd.Run()
 
-		wg.Add(1)
-		go func(path string) {
-			defer wg.Done()
+	return err
 
-			cmd := exec.Command("pnputil.exe", "/add-driver", path, "/install")
-			if logFile != nil {
-				cmd.Stdout = logFile
-				cmd.Stderr = logFile
-			}
+}
 
-			if err := cmd.Run(); err != nil {
-				atomic.AddUint64(&failedInstalls, 1)
-			} else {
-				atomic.AddUint64(&successfulInstalls, 1)
-			}
-			bar.Add(1)
-		}(path)
-	}
+func printStats(success uint64, failed uint64) {
 
-	wg.Wait()
+	fmt.Printf("Успешно установлено: %d, Установить не удалось: %d\n", success, failed)
 
-	bar.Finish()
-
-	fmt.Printf("Successful installs: %d, Failed installs: %d\n", successfulInstalls, failedInstalls)
-	fmt.Printf("Driver installation is complete, rebooting is recommended. Press any button to exit.")
+	fmt.Print("Установка драйверов завершена. Для выхода нажмите любую клавишу.")
 	fmt.Scanln()
+
 }
